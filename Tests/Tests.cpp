4ecb3605911c51826c6aea67bd8cc99e03b80b95
Korney Czukowski <carbofos@seznam.cz>
2012-10-10 18:16:58 +0200

    Added Tests project to solution

---
+++ Tests/Tests.cpp
@@ -0,0 +1,14 @@
+/**
+ * Tests.cpp - Tests runner
+ * 
+ * Author:     Korney Czukowski (https://github.com/czukowski)
+ * Copyright:  (c) 2012 Korney Czukowski
+ * License:    MIT License
+ */
+
+#include <ArduinoUnit.h>
+
+int main()
+{
+    
+}
